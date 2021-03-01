39609239d9cc39c0313c943f16351d0f9a1472c6
Tu Tran <tutn.stu09@gmail.com>
2021-03-01 22:54:31 +0700

    profile ud

--- reponsitories/index.js
+++ reponsitories/index.js
@@ -2,7 +2,7 @@ const mongodb = require("mongodb");
 
 const db = {};
 
-const client = new mongodb.MongoClient("mongodb://localhost:27017");
+const client = new mongodb.MongoClient("mongodb+srv://admin:1@cluster0.xmufk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
 client.connect().then((connectedClient) => {
     console.log("mongodb connected!");
     const database = connectedClient.db("Xfuri");
